c32862ce6edc06844c984be2e06caea78b572633
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-08-22 13:48:24 +0700

    JavaScript Extenal

---
+++ 1.JavaScriptExtenal/light.js
@@ -0,0 +1,8 @@
+function turnLightOn(){
+    var img = document.getElementById("myImage");
+    img.src = "light-on.gif"
+}
+function turnLightOff(){
+    var img = document.getElementById("myImage");
+    img.src ="light-off.gif";
+}
